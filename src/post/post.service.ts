10872b9f2a8aa4fec4d757aa07faa98f26fbfbc0
UnderSaga <98108699+UnderSaga@users.noreply.github.com>
2024-03-27 20:37:39 +0300

    Изменения: - Отработана ошибка при  обновлении статьи.

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,10 +1,10 @@
-import { Injectable } from "@nestjs/common"
-import { InjectModel } from "@nestjs/mongoose"
-import { User } from "src/schemas/user.schema"
-import { Model } from "mongoose"
-import { PostDto } from "./post.dto"
-import { Post } from "src/schemas/post.schema"
-import { Response } from "express"
+import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { User } from "src/schemas/user.schema";
+import { Model } from "mongoose";
+import { PostDto } from "./post.dto";
+import { Post } from "src/schemas/post.schema";
+import { Response } from "express";
 
 @Injectable()
 export class PostService {
@@ -20,28 +20,28 @@ export class PostService {
         body: postDto.body,
         tags: postDto.tags,
         imageUrl: postDto.imageUrl,
-      })
+      });
 
-      await doc.save()
+      await doc.save();
 
-      res.json(doc)
+      res.json(doc);
     } catch (err) {
-      console.error(err)
+      console.error(err);
       res.status(500).json({
         message: "Не удалось создать статью.",
-      })
+      });
     }
   }
 
   async getAll(res: Response) {
     try {
-      const posts = await this.postModel.find()
+      const posts = await this.postModel.find();
 
-      res.json(posts)
+      res.json(posts);
     } catch (error) {
       res.status(500).json({
         error: "Не удалось получить список статей.",
-      })
+      });
     }
   }
 
@@ -63,21 +63,21 @@ export class PostService {
           if (!doc) {
             return res.status(404).json({
               message: "Статья не найдена.",
-            })
+            });
           }
 
-          res.json(doc)
-        })
+          res.json(doc);
+        });
     } catch (error) {
       res.status(500).json({
         error: "Не удалось получить список статей.",
-      })
+      });
     }
   }
 
   async updatePost(dto: PostDto, id: string, res: Response) {
     try {
-      const post = await this.postModel
+      await this.postModel
         .findByIdAndUpdate(
           {
             _id: id,
@@ -93,29 +93,34 @@ export class PostService {
           if (!doc) {
             return res.status(404).json({
               message: "Статья не найдена",
-            })
+            });
           }
 
-          res.json({ message: "Статья успешно обновлена" })
-        })
-    } catch (error) {}
+          res.json({ message: "Статья успешно обновлена" });
+        });
+    } catch (error) {
+      res.status(500).json({
+        message:
+          "Не удалось обновить статью. Возможно, статья с таким заголовком уже есть.",
+      });
+    }
   }
 
   async deletePost(id: string, res: Response) {
     try {
       await this.postModel.findByIdAndDelete(id).then((doc) => {
         if (!doc) {
-          throw new Error()
+          throw new Error();
         }
 
         res.json({
           message: "Статья успешно удалена",
-        })
-      })
+        });
+      });
     } catch (error) {
       res.status(500).json({
         error: "Не удалось получить список статей.",
-      })
+      });
     }
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,12 +1,12 @@
-import { Injectable, UnauthorizedException } from "@nestjs/common"
-import { InjectModel } from "@nestjs/mongoose"
-import { User } from "src/schemas/user.schema"
-import { Model } from "mongoose"
-import { UserDto } from "./user.dto"
-import { JwtService } from "@nestjs/jwt"
-import * as bcrypt from "bcryptjs"
-import { Role } from "src/schemas/role.schema"
-import { Request, Response } from "express"
+import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { User } from "src/schemas/user.schema";
+import { Model } from "mongoose";
+import { UserDto } from "./user.dto";
+import { JwtService } from "@nestjs/jwt";
+import * as bcrypt from "bcryptjs";
+import { Role } from "src/schemas/role.schema";
+import { Request, Response } from "express";
 
 @Injectable()
 export class UserService {
@@ -18,60 +18,61 @@ export class UserService {
 
   async create(userDto: UserDto, res: Response) {
     try {
-      const { username, email, password } = userDto
-      const candidate = await this.userModel.findOne({ email: email })
+      const { username, email, password } = userDto;
+      const candidate = await this.userModel.findOne({ email: email });
+
       if (candidate) {
-        throw new Error()
+        throw new Error();
       }
-      const hash = await bcrypt.hash(password, 10)
-      const userRole = await this.roleModel.findOne({ value: "USER" })
+      const hash = await bcrypt.hash(password, 10);
+      const userRole = await this.roleModel.findOne({ value: "USER" });
 
       const user = new this.userModel({
         email,
         username,
         password: hash,
         roles: [userRole.value],
-      })
+      });
 
-      user.save()
+      user.save();
 
       const token = this.jwtService.sign({
         _id: user._id,
         name: user.username,
-      })
+      });
 
-      res.json({ token })
+      res.json({ token });
     } catch (error) {
       res.status(500).json({
         error: "Не удалось создать пользователя.",
-      })
+      });
     }
   }
 
   async login(userDto: UserDto, res: Response) {
     try {
-      const user = await this.userModel.findOne({ email: userDto.email })
+      const user = await this.userModel.findOne({ email: userDto.email });
 
       if (!user) {
         res.status(404).json({
           message: "Пользователь не найден.",
-        })
+        });
       }
 
-      const isValidPass = await bcrypt.compare(userDto.password, user.password)
+      const isValidPass = await bcrypt.compare(userDto.password, user.password);
 
       if (!isValidPass) {
         return res.status(400).json({
           message: "Неверный логин или пароль",
-        })
+        });
       }
 
       const token = this.jwtService.sign({
         _id: user._id,
         name: user.username,
-      })
+      });
 
-      const { _id, username, email, roles } = user
+      const { _id, username, email, roles } = user;
 
       res.json({
         _id,
@@ -79,31 +80,31 @@ export class UserService {
         email,
         roles,
         token,
-      })
+      });
     } catch (error) {
       res.status(500).json({
         error: "Не удалось войти в учетную запись.",
-      })
+      });
     }
   }
 
   async getMe(req: Request, res: Response) {
     try {
-      const token = (req.headers.authorization || "").replace(/Bearer\s?/, "")
-      const decoded = await this.jwtService.verify(token)
+      const token = (req.headers.authorization || "").replace(/Bearer\s?/, "");
+      const decoded = await this.jwtService.verify(token);
       const user = await this.userModel.findOne({
         _id: decoded._id,
-      })
+      });
 
       if (!user) {
-        throw new UnauthorizedException()
+        throw new UnauthorizedException();
       }
 
-      res.json(user)
+      res.json(user);
     } catch (error) {
       res.status(500).json({
         error: "Что-то пошло не так.",
-      })
+      });
     }
   }
 }
