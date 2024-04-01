05a3558a713b99162cdfc50f3f33d720e090e2b7
UnderSaga <undersaga@mail.ru>
2024-04-01 16:06:44 +0300

    Изменения: - Теперь в проекте существует логгер, который в теории пишет что-то в консоль и точно создает файлы под разные уровни информации (info, warning и прочее)

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { UserModule } from "./user/user.module"
 import { PostModule } from "./post/post.module"
 import { CommentModule } from "./comment/comment.module"
 import { MongooseModule } from "@nestjs/mongoose"
+import { WinstonModule } from "nest-winston"
+import * as winston from "winston"
 
 @Module({
   imports: [
@@ -12,6 +14,39 @@ import { MongooseModule } from "@nestjs/mongoose"
     MongooseModule.forRoot(
       "mongodb+srv://undersaga:Uq7123546E@mydb.zlgzmkd.mongodb.net/?retryWrites=true&w=majority&appName=mydb"
     ),
+    WinstonModule.forRoot({
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json()
+      ),
+      transports: [
+        new winston.transports.Console(),
+        new winston.transports.File({
+          dirname: "src/../log/debug/",
+          filename: "debug.log",
+          level: "debug",
+          maxsize: Number(process.env.MAX_SIZE_FOR_LOGS),
+        }),
+        new winston.transports.File({
+          dirname: "src/../log/warning/",
+          filename: "warning.log",
+          level: "warn",
+          maxsize: Number(process.env.MAX_SIZE_FOR_LOGS),
+        }),
+        new winston.transports.File({
+          dirname: "src/../log/info/",
+          filename: "info.log",
+          level: "log",
+          maxsize: Number(process.env.MAX_SIZE_FOR_LOGS),
+        }),
+        new winston.transports.File({
+          dirname: "src/../log/error/",
+          filename: "error.log",
+          level: "error",
+          maxsize: Number(process.env.MAX_SIZE_FOR_LOGS),
+        }),
+      ],
+    }),
   ],
   controllers: [],
   providers: [],
--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,14 @@ import { AppModule } from "./app.module"
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger"
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create(AppModule, {
+    logger: ["error", "warn", "debug", "log"],
+  })
 
   const config = new DocumentBuilder()
     .setTitle("Back API")
     .setDescription("Описание всех апишек бэка")
     .setVersion("1.0")
-    .addTag("User")
-    .addTag("Post")
-    .addTag("Comment")
     .build()
   const document = SwaggerModule.createDocument(app, config)
   SwaggerModule.setup("swagger", app, document)
